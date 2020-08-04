e43f667f9e1ffffe529180ddbf5794535f0805aa
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-08-04 11:51:28 -0400

    fixed fab

--- src/components/Admin/StoreAdmin.js
+++ src/components/Admin/StoreAdmin.js
@@ -14,6 +14,7 @@ import {
   Hidden,
   Fab,
   Modal,
+  Container,
 } from "@material-ui/core";
 // import DeleteIcon from "@material-ui/icons/Delete";
 // import EditIcon from "@material-ui/icons/Edit";
@@ -88,6 +89,13 @@ const useStyles = makeStyles((theme) => ({
 
   modal: {
     marginTop: theme.spacing(2),
+    overflow: "scroll",
+  },
+
+  fab: {
+    position: "fixed",
+    right: theme.spacing(1),
+    bottom: theme.spacing(2),
   },
 
   toolbar: theme.mixins.toolbar,
